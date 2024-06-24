db52a412f589f455e5df8057acb7bca97ec11001
choiyeon <2dpaesu@gmail.com>
2024-06-24 17:23:06 +0900

    찜 기능

--- meonggae_prj/src/main/java/com/store/meonggae/product/service/ProductDetailInfoService.java
+++ meonggae_prj/src/main/java/com/store/meonggae/product/service/ProductDetailInfoService.java
@@ -21,4 +21,25 @@ public class ProductDetailInfoService {
     	int checkResult = pdiDAO.checkMemSteam(steamVO);
         return checkResult == 1;//찜했으면 true 아니면 false
     }
+    
+    //찜 등록
+    public int insertSteam(SteamVO steamVO) {
+    	int insertSteam = 0;
+    	//있다가 삭제되었던 값이 있는지 확인. 있으면 다시찜. 없으면 첫찜.
+    	int checkResult = pdiDAO.checkFirstSteam(steamVO);
+    	//첫 찜
+    	if(checkResult == 0) {
+    		insertSteam = pdiDAO.insertSteam(steamVO);
+    	}else if(checkResult == 1){
+    	//삭제했다가 다시 찜.
+    		insertSteam = pdiDAO.updateSteamToY(steamVO);
+    	}
+    	return insertSteam;
+    }
+    
+    //찜 삭제
+    public int updateSteamToN(SteamVO steamVO) {
+    	return pdiDAO.updateSteamToN(steamVO);
+    }
+    
 }
